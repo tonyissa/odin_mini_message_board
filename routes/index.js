c53714a76223cc146381f37b9677028cdd45465b
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-09-27 18:26:06 -0400

    Add basic views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Armando",
+    added: new Date().toLocaleDateString()
+  },
+  {
+    text: "Hi World!",
+    user: "Charles",
+    added: new Date().toLocaleDateString()
+  }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: "Mini Messageboard" })
 });
 
 module.exports = router;
