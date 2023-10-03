899833e54b8cbffa48d763c940ba020b223ce8a9
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-10-03 14:21:37 -0400

    Code updating database with new messages

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const messages = await PostModel.find()
+  const messages = await PostModel.find().sort({ date: -1 });
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
@@ -13,9 +13,13 @@ router.get('/new', function(req, res, next) {
   res.render('form', { title: "Mini Messageboard" })
 });
 
-router.post('/new', function(req, res, next) {
-  messages.push({text: req.body.text, user: req.body.author, added: new Date().toLocaleDateString()})
-  res.redirect('/');
+router.post('/new', async function(req, res, next) {
+  try {
+    await PostModel.create({ text: req.body.text, user: req.body.user, date: new Date()})
+    res.redirect('/');
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
